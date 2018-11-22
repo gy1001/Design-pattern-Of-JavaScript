840f66112091a584e7a22148055ed8efda5942a6
gaoyuan <gaoyuan_flying@163.com>
2018-11-22 17:54:36 +0800

    第一题面试题

--- 03-设计原则/demo/src/index.js
+++ 03-设计原则/demo/src/index.js
@@ -1,31 +1,70 @@
-function loadImg(src) {
-  let promise = new Promise(function(resolve, reject) {
-    let img = document.createElement("img");
-    img.onload = function() {
-      resolve(img);
-    };
-    img.onerror = function(img) {
-      reject("图片加载失败");
-    };
-    img.src = src;
-  });
-  return promise;
+// function loadImg(src) {
+//   let promise = new Promise(function(resolve, reject) {
+//     let img = document.createElement("img");
+//     img.onload = function() {
+//       resolve(img);
+//     };
+//     img.onerror = function(img) {
+//       reject("图片加载失败");
+//     };
+//     img.src = src;
+//   });
+//   return promise;
+// }
+// const src = "https://www.imooc.com/static/img/index/logo.png";
+// let result = loadImg(src);
+// result
+//   .then(function(img) {
+//     // part 1
+//     console.log("img.width", img.width);
+//     return img;
+//   })
+//   .then(function(img) {
+//     // part 2
+//     console.log("img.height", img.height);
+//   })
+//   .then(function() {
+//     //part 3
+//   })
+//   .catch(function(error) {
+//     console.log(error);
+//   });
+
+class Car {
+  constructor(name, number) {
+    this.name = name;
+    this.number = number;
+  }
 }
-const src = "https://www.imooc.com/static/img/index/logo.png";
-let result = loadImg(src);
-result
-  .then(function(img) {
-    // part 1
-    console.log("img.width", img.width);
-    return img;
-  })
-  .then(function(img) {
-    // part 2
-    console.log("img.height", img.height);
-  })
-  .then(function() {
-    //part 3
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
+
+class Kuaiche extends Car {
+  constructor(name, number) {
+    super(name, number);
+    this.price = 1;
+  }
+}
+
+class Zhuanche extends Car {
+  constructor(name, number) {
+    super(name, number);
+    this.price = 2;
+  }
+}
+
+// 行程
+class Trip {
+  constructor(car) {
+    this.car = car;
+  }
+  start() {
+    console.log(`车名 ${this.car.name} 车牌号${this.car.number}`);
+  }
+  end() {
+    console.log(`行程结束，价格${this.car.price * 5}`);
+  }
+}
+
+let car = new Kuaiche(100, "桑塔纳");
+let trip = new Trip(car);
+trip.start();
+trip.end();
--- 03-设计原则/index.md
+++ 03-设计原则/index.md
@@ -178,7 +178,7 @@ result.then(function(img){
 
 ###　第一题
 
-> - 打车时，可以打专车或者快车，任何撤都有车牌和名称。
+> - 打车时，可以打专车或者快车，任何车都有车牌和名称。
 > - 不同车价格不同，快车每公里１元，专车每公里２元。
 > - 行程开始时候，显示车辆信息
 > - 行程结束时候，显示打车金额，（假定行程就５公里）
@@ -187,3 +187,39 @@ result.then(function(img){
 
 > - 画出 UML 类图
 > - 用 ES6 语法写出该示例
+
+```javascript
+class Car{
+  constructor(number,name){
+    this.number =number;
+    this.name = name;
+  }
+}
+class Kuaiche extends Car{
+  constructor(number, name){
+    super(number,name)
+    this.price = 1
+  }
+}
+class Zhuanche extend Car{
+  constructor(number,name){
+    super(number,name)
+    this.price = 2
+  }
+}
+class Trip{
+  constructor(car){
+    this.car = car;
+  }
+  start(){
+    console.log(`行程开始，名称${this.car.name},车牌号${this.car.number}`)
+  }
+  end(){
+    console.log(`行程结束,价格${this.car.price} * 5`)
+  }
+}
+let car = new Kuaiche (100,'桑塔纳');
+let trip = new Trip(car);
+trip.start();
+trip.end();
+```
