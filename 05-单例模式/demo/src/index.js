e0ba0c7b2440799d6bd0656b7a9ec37296775c2e
gaoyuan <gaoyuan_flying@163.com>
2018-11-25 18:50:18 +0800

    模拟登录框代码演示

--- 05-单例模式/demo/src/index.js
+++ 05-单例模式/demo/src/index.js
@@ -1,24 +1,40 @@
-class SingleObject {
-  login() {
-    console.log("login...");
+class LoginForm {
+  constructor() {
+    this.state = "hide";
+  }
+  show() {
+    if (this.state === "show") {
+      alert("已经显示");
+      return;
+    }
+    this.state = "show";
+    console.log("登录框显示成功");
+  }
+  hide() {
+    if (this.state === "hide") {
+      alert("登录框已经隐藏");
+      return;
+    }
+    this.state = "hide";
+    console.log("登录框隐藏成功");
   }
 }
 
-SingleObject.getInstance = (function() {
+LoginForm.getInstance = (function() {
   let instance;
   return function() {
     if (!instance) {
-      instance = new SingleObject();
+      instance = new LoginForm();
     }
     return instance;
   };
 })();
 
-let obj1 = SingleObject.getInstance();
-obj1.login();
-let obj2 = SingleObject.getInstance();
-obj2.login();
-console.log("obj1===0bj2", obj1 === obj2); // true
-let obj3 = new SingleObject(); //这一点在ｊｓ中是无法控制的
-obj3.login();
-console.log("obj1===obj3", obj1 === obj3);
+// 测试
+let login1 = LoginForm.getInstance();
+login1.show();
+
+let login2 = LoginForm.getInstance();
+login2.hide();
+
+console.log(login1 === login2);
