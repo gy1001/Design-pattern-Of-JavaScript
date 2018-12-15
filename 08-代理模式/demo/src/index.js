564078026daa98bec1f3397788940092dd858fda
gaoyuan <gaoyuan_flying@163.com>
2018-12-15 20:56:52 +0800

    代理模式

--- 08-代理模式/demo/src/index.js
+++ 08-代理模式/demo/src/index.js
@@ -1,27 +1,73 @@
-class ReadImg {
-  constructor(fileName) {
-    this.fileName = fileName;
-    this.loadFromDisk(); // 初始化即从硬盘中加载，模拟
-  }
+// class ReadImg {
+//   constructor(fileName) {
+//     this.fileName = fileName;
+//     this.loadFromDisk(); // 初始化即从硬盘中加载，模拟
+//   }
 
-  display() {
-    console.log("display..." + this.fileName);
-  }
+//   display() {
+//     console.log("display..." + this.fileName);
+//   }
 
-  loadFromDisk() {
-    console.log("loading..." + this.fileName);
-  }
-}
+//   loadFromDisk() {
+//     console.log("loading..." + this.fileName);
+//   }
+// }
 
-class ProxyImg {
-  constructor(fileName) {
-    this.realImg = new ReadImg(fileName);
-  }
-  display() {
-    this.realImg.display();
-  }
-}
+// class ProxyImg {
+//   constructor(fileName) {
+//     this.realImg = new ReadImg(fileName);
+//   }
+//   display() {
+//     this.realImg.display();
+//   }
+// }
 
-// 测试
-let proxyImg = new ProxyImg("1.png");
-proxyImg.display();
+// // 测试
+// let proxyImg = new ProxyImg("1.png");
+// proxyImg.display();
+
+// 明星经纪人
+// 明细
+let star = {
+  name: "张xx",
+  age: 25,
+  phone: "13900000111"
+};
+
+// 经纪人
+let agent = new Proxy(star, {
+  get: function(target, key) {
+    if (key === "phone") {
+      // 明星经纪人电话
+      return "1688ddddgg";
+    }
+    if (key === "price") {
+      // 明星不报价，经纪人报价
+      return 120000;
+    }
+    return target[key];
+  },
+  set: function(target, key, val) {
+    if (key === "customePrice") {
+      if (val < 100000) {
+        //  最低10w
+        throw new Error("价格太低");
+      } else {
+        target[key] = val;
+        return true;
+      }
+    }
+  }
+});
+
+//  测试
+console.log(agent.name);
+console.log(agent.age);
+console.log(agent.phone);
+console.log(agent.price);
+agent.customePrice = 112000;
+
+console.log(agent.customePrice);
+
+agent.customePrice = 5000;
+console.log(agent.customePrice);
--- 08-代理模式/index.md
+++ 08-代理模式/index.md
@@ -5,4 +5,88 @@
 
 <!-- 示例：科学上网，明星经纪人 -->
 
-> -
+## 场景
+
+> - 网页事件代理
+
+<!-- 子元素的点击事件由父元素代理 -->
+
+> - JQuerry \$.proxy
+
+<!--
+$("#div1").click(function(){
+  // this符合期望
+  $(this).addClass("red")
+})
+
+$("#div1").click(function(){
+  setTimeout(function(){
+    // this不符合期望
+    $(this).addClass("red")
+  },100)
+})
+ -->
+
+ <!-- 可以用如下方式解决
+ $("#div1").click(function(){
+  // this符合期望
+  $(this).addClass("red")
+})
+
+$("#div1").click(function(){
+  var _this = this;
+  setTimeout(function(){
+    // _this符合期望
+    $(_this).addClass("red")
+  },100)
+})
+  -->
+
+<!-- 但是推荐使用$.proxy 来解决，这样就少定义一个变量
+$("#div1").click(function(){
+  var _this = this;
+  setTimeout($.proxy(function(){
+    // this符合期望
+    $(this).addClass("red")
+  },this),100)
+})
+ -->
+
+> - ES6 proxy
+
+<!-- 明星经纪人
+// 明星
+
+let star = {
+name:"张 xx",
+age:25,
+phone:'15138971750'
+}
+
+//经纪人
+let agent = new Proxy(star,{
+  get: function(targe,key){
+    if(key ==="phone"){
+    //返回经纪人自己的手机号码
+      return "188888888***"
+    }
+    if(key==="price"){
+       // 明星不报价，经纪人报价
+      return 120000
+    }
+    return targe[key]
+  },
+  set: function(target,key,val){
+    if(key ==="customPrice"){
+      if(val<100000){
+        throw new Error("价格太低")
+      }else {
+        taget[key] = val
+        return true
+      }
+    }
+  }
+})
+
+
+-->
